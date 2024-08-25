630ccb75574a65a4cbe07d8fbe8976b0bca1e89c
RishiJ <120023081+Rishi7762@users.noreply.github.com>
2024-08-25 18:56:26 +0530

    Update Remainder.java

--- Assignments/Remainder.java
+++ Assignments/Remainder.java
@@ -1,1 +1,22 @@
-
+import java.util.*;
+class Remainder{
+    public static void main(String[] args){
+    int dividend ,divisor;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter dividend: ");
+    dividend = sc.nextInt();
+			
+    System.out.println("Enter divisor: ");
+    divisor = sc.nextInt();
+			
+    if(divisor<=0)
+        System.out.println("Cannot divide by zero");
+			
+    while(dividend >= divisor){
+        dividend  = dividend - divisor;
+    }
+			
+    int remainder = dividend;
+    System.out.println("The Remainder is: "+remainder);
+    }
+}
