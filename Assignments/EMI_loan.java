815e9ba51523233aa24dc2f7d73cf34006fdf5c6
RishiJ <120023081+Rishi7762@users.noreply.github.com>
2024-09-02 19:36:34 +0530

    Update EMI_loan.java

--- Assignments/EMI_loan.java
+++ Assignments/EMI_loan.java
@@ -1,26 +1,34 @@
 package demo;
-
 import java.util.Scanner;
-
 public class EMI_loan {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 
-			// TODO Auto-generated method stub
-			System.out.println("Calculte the EMI_ioan");
-			int price,time,rate,n=100;
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter the price:");
-			price=sc.nextInt();
-			System.out.println("Enter the rate:");
-			rate=sc.nextInt(); 
-			System.out.println("Enter the time:");
-			time=sc.nextInt();
-			time=time*12 ;
-			int loan =(price*rate*(1+rate)^time)/((1+rate)^time-1);
-			System.out.println("Emi_loan :"+loan);
-		
-	}
+    public static void main(String[] args) {
+        // Start of the EMI calculator program
+        System.out.println("Calculate the EMI loan");
 
+        // Declare variables to store price, time, and rate
+        int price, time, rate, n = 100;
+        Scanner sc = new Scanner(System.in);
+
+        // Get the loan amount from the user
+        System.out.println("Enter the price:");
+        price = sc.nextInt();
+
+        // Get the rate of interest
+        System.out.println("Enter the rate:");
+        rate = sc.nextInt();
+
+        // Get the loan period (in years)
+        System.out.println("Enter the time:");
+        time = sc.nextInt();
+
+        // Convert years to months
+        time = time * 12;
+
+        // Calculate the EMI using a formula
+        int loan = (price * rate * (1 + rate) ^ time) / ((1 + rate) ^ time - 1);
+
+        // Show the calculated EMI
+        System.out.println("EMI loan: " + loan);
+    }
 }
