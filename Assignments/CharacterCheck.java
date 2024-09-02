03829e87718608782ff641fcc8c3e7f5c98abecf
RishiJ <120023081+Rishi7762@users.noreply.github.com>
2024-09-02 19:35:00 +0530

    Update CharacterCheck.java

--- Assignments/CharacterCheck.java
+++ Assignments/CharacterCheck.java
@@ -5,16 +5,19 @@ public class CharacterCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Accept a character from the user
+        // Ask for a character
         System.out.print("Enter a character: ");
-        char ch = sc.next().charAt(0);
+        char ch = sc.next().charAt(0);  // Reads the character
 
+        // Check if it's a digit
         if (Character.isDigit(ch)) {
             System.out.println(ch + " is a digit.");
         }
+        // Check if it's a letter
         else if (Character.isLetter(ch)) {
             System.out.println(ch + " is a letter.");
         }
+        // Otherwise, it's a special symbol
         else {
             System.out.println(ch + " is a special symbol.");
         }
