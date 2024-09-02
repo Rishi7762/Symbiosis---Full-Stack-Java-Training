ccf771ae5a94dda4703c02c6c951e4ed8b7fbfec
RishiJ <120023081+Rishi7762@users.noreply.github.com>
2024-09-02 19:37:17 +0530

    Update Employee.java

--- Assignments/Employee.java
+++ Assignments/Employee.java
@@ -1,39 +1,46 @@
 package demo;
 import java.util.*;
-public class Employee {
-	int personId,age;String personName;
-	
-	Employee(int personId,int age,String personName){
-		this.personId = personId;
-		this.age = age;
-		this.personName = personName;
-	}
-	
-	public void display() {
-		System.out.println("Person Id is : "+personId);
-		System.out.println("Person age is : "+age);
-		System.out.println("Person Name is : "+personName);
-	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Employee emp[] = new Employee[4];
-		int id,age;String name;
-		Scanner sc = new Scanner(System.in);
-		
-		//input for person
-		for(int i = 0;i<emp.length;i++) {
-			System.out.println("Enter person id: ");
-			id = sc.nextInt();
-			System.out.println("Enter person age: ");
-			age = sc.nextInt();
-			System.out.println("Enter person name: ");
-			name=sc.next();
-			emp[i] = new Employee(id,age,name);
-		}
-		//displaying
-		for(Employee i:emp) {
-			i.display();
-		}
-	}
+public class Employee {
+    int personId, age;
+    String personName;
+
+    // Constructor to initialize employee details
+    Employee(int personId, int age, String personName) {
+        this.personId = personId;
+        this.age = age;
+        this.personName = personName;
+    }
+
+    // Method to display employee details
+    public void display() {
+        System.out.println("Person Id is: " + personId);
+        System.out.println("Person age is: " + age);
+        System.out.println("Person Name is: " + personName);
+    }
+
+    public static void main(String[] args) {
+        // Array to hold multiple Employee objects
+        Employee emp[] = new Employee[4];
+        int id, age;
+        String name;
+        Scanner sc = new Scanner(System.in);
+
+        // Get input for each person
+        for (int i = 0; i < emp.length; i++) {
+            System.out.println("Enter person id: ");
+            id = sc.nextInt();
+            System.out.println("Enter person age: ");
+            age = sc.nextInt();
+            System.out.println("Enter person name: ");
+            name = sc.next();
+            // Create a new Employee object with provided details
+            emp[i] = new Employee(id, age, name);
+        }
+
+        // Display all employees' details
+        for (Employee i : emp) {
+            i.display();
+        }
+    }
 }
