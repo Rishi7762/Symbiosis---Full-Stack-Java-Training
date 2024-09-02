618b44d9987edd467de3bf8c05e7112e0dd9fd8e
RishiJ <120023081+Rishi7762@users.noreply.github.com>
2024-09-02 21:15:43 +0530

    Update and rename convertonfor.java to ConversionFor.java

--- Assignments/ConversionFor.java
+++ Assignments/ConversionFor.java
@@ -1,33 +1,40 @@
 package demo;
 
-public class convertonfor {
+public class ConversionFor {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
         int sum = 0;
+        // Create a 3x5 2D array
         int num[][] = new int[3][5];
 
+        // Fill the array with multiplication values
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 5; j++) {
                 num[i][j] = (i + 1) * (j + 1);
             }
         }
+
+        // Print the 2D array
         System.out.println("Print 2D array:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.print(num[i][j] + " ");
             }
-            System.out.println();
+            System.out.println(); // Move to the next line after each row
         }
+
+        // Print a separator
         System.out.println("-------------------------------");
+
+        // Calculate and print each value in the array and its sum
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 5; j++) {
-            	System.out.println("Value is:"+num[i][j]);
+                System.out.println("Value is: " + num[i][j]);
                 sum += num[i][j];
             }
         }
 
+        // Print the total sum of all values
         System.out.println("Sum of all values: " + sum);
     }
 }
-
