b720702d70c1faca64fa31491687a87e68edb679
RishiJ <120023081+Rishi7762@users.noreply.github.com>
2024-09-02 21:31:03 +0530

    Update stud.java

--- Assignments/stud.java
+++ Assignments/stud.java
@@ -1,27 +1,52 @@
 package demo;
-import java.util.*;
 
-public class stud {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Arithmatic Calculation");
-			int num1,num2;
-			Scanner sc= new Scanner(System.in);
-			num1=sc.nextInt();
-			num2=sc.nextInt();
-			int sum = num1+num2;
-			System.out.println("Addition is :"+sum);
-			int sub = num1-num2;
-			System.out.println("substraction is :"+sub);
-			int div = num1/num2;
-			System.out.println("Division is :"+div);
-			int mul = num1*num2;
-			System.out.println("Multipication is :"+mul);
-			int mod = num1%num2;
-			System.out.println("Remander is :"+mod);
-			
+public class Stud {
 
-	}
+    public static void main(String[] args) {
+        // Display the purpose of the program
+        System.out.println("Arithmetic Calculations");
 
+        // Declare variables for input numbers
+        int num1, num2;
+
+        // Create a Scanner object for input
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.println("Enter the first number:");
+        num1 = sc.nextInt();
+        System.out.println("Enter the second number:");
+        num2 = sc.nextInt();
+
+        // Perform arithmetic operations and display results
+        int sum = num1 + num2;
+        System.out.println("Addition is: " + sum);
+
+        int sub = num1 - num2;
+        System.out.println("Subtraction is: " + sub);
+
+        // Check for division by zero
+        if (num2 != 0) {
+            int div = num1 / num2;
+            System.out.println("Division is: " + div);
+        } else {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+
+        int mul = num1 * num2;
+        System.out.println("Multiplication is: " + mul);
+
+        // Check for modulo by zero
+        if (num2 != 0) {
+            int mod = num1 % num2;
+            System.out.println("Remainder is: " + mod);
+        } else {
+            System.out.println("Error: Modulo by zero is not allowed.");
+        }
+
+        // Close the Scanner
+        sc.close();
+    }
 }
