8a436040f19a88ac479cda2f42a3b189ab1f135e
RishiJ <120023081+Rishi7762@users.noreply.github.com>
2024-09-02 21:31:39 +0530

    Update sumofarray.java

--- Assignments/sumofarray.java
+++ Assignments/sumofarray.java
@@ -1,23 +1,34 @@
 package demo;
-import java.util.*;
-public class sumofarray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int num;
-		int sum=0;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the size of array:");
-		num= sc.nextInt();
-		int[] arr=new int[num];
-		System.out.println("Enter the elements :");
-		for(int i=0;i<num;i++)
-		{
-			arr[i]=sc.nextInt();
-			sum += arr[i];
-		}
-		System.out.println("The sum of array element is :"+sum);
-		
-	}
+import java.util.Scanner;
 
+public class SumOfArray {
+
+    public static void main(String[] args) {
+        // Initialize sum variable
+        int sum = 0;
+
+        // Create a Scanner object for input
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user for the size of the array
+        System.out.println("Enter the size of the array:");
+        int num = sc.nextInt();
+
+        // Initialize the array
+        int[] arr = new int[num];
+
+        // Prompt user for array elements
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt(); // Read each element
+            sum += arr[i];         // Add element to sum
+        }
+
+        // Display the sum of array elements
+        System.out.println("The sum of the array elements is: " + sum);
+
+        // Close the Scanner
+        sc.close();
+    }
 }
